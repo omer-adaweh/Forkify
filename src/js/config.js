9c7aabafc0a64d6378b6fa7f0aeac726e22f1a94
Omer <adaweh213@gmail.com>
2021-08-30 13:22:40 -0400

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '470bfce5-fa3c-440c-8330-2fcb945a5698';
+export const MODAL_CLOSE_SEC = 2.5;
